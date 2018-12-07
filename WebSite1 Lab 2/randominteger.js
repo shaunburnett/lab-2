7189f6e88ed469058239e7b3ab98c2a613b4ee93
shaunburnett <45385900+shaunburnett@users.noreply.github.com>
2018-12-07 14:09:22 +1300

    Add files via upload

---
+++ WebSite1 Lab 2/randominteger.js
@@ -0,0 +1,16 @@
+﻿
+function myNumber() {
+
+    var y = Math.floor(Math.random() * (10)) + 1;
+    document.getElementById("randomInt").innerHTML = "Task 3 = Result of random generated number is " + y;
+    return;
+}
+
+
+function myRange(min, max) {
+
+    var z = Math.floor(Math.random() * (max - min)) + min;
+    document.getElementById("range").innerHTML = "Task 4 = Result of range numbers are " + z;
+    return;
+}
+
